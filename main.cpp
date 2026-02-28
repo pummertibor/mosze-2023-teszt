c54975891c795f4fc1ebf4f34e90513105556821
Pummer Tibor <pummer.tibor@hallgato.sze.hu>
2026-02-28 15:08:43 +0100

    Fixing problems

--- main.cpp
+++ main.cpp
@@ -4,25 +4,26 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //  rossz konstansnév
-    std::cout << '1-100 ertekek duplazasa' // rossz idézőjelek, hiányzó lezárás (endl) és pontosvessző
-    for (int i = 0;) //hiányzó teljes for ciklus, 1-től N_ELEMENTS-ig (100-ig)
+    int *b = new int[N_ELEMENTS]; //  rossz konstansnév
+    std::cout << "1-100 ertekek duplazasa" << std::endl; // rossz idézőjelek, hiányzó lezárás (endl) és pontosvessző
+    for (int i = 1; i <= N_ELEMENTS; i++) //hiányzó teljes for ciklus, 1-től N_ELEMENTS-ig (100-ig)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) //hiányzó feltétel
+    for (int i = 1; i <= N_ELEMENTS; i++) //hiányzó feltétel
     {
-        std::cout << "Ertek:" //hiányzó lezárás és pontosvessző
+        std::cout << "Ertek:" << b[i] << std::endl; //hiányzó lezárás és pontosvessző
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; //hiányzó inicializálás
-    for (int i = 0; i < N_ELEMENTS, i++) //pontosvessző helyett vessző  
+    int atlag = 0; //hiányzó inicializálás
+    for (int i = 1; i <= N_ELEMENTS; i++) //pontosvessző helyett vessző  
     {
-        atlag += b[i] //hiányzó pontosvessző
+        atlag += b[i]; //hiányzó pontosvessző
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
 
     //lefoglalt memória felszabadítása
+    delete[] b;
     return 0;
 }
