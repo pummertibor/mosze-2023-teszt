da1def080738a567b0f314424b7122255b74c9a6
Pummer Tibor <pummer.tibor@hallgato.sze.hu>
2026-02-28 15:00:29 +0100

    Add error comments

--- main.cpp
+++ main.cpp
@@ -4,23 +4,25 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //  rossz konstansnév
+    std::cout << '1-100 ertekek duplazasa' // rossz idézőjelek, hiányzó lezárás (endl) és pontosvessző
+    for (int i = 0;) //hiányzó teljes for ciklus, 1-től N_ELEMENTS-ig (100-ig)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //hiányzó feltétel
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //hiányzó lezárás és pontosvessző
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //hiányzó inicializálás
+    for (int i = 0; i < N_ELEMENTS, i++) //pontosvessző helyett vessző  
     {
-        atlag += b[i]
+        atlag += b[i] //hiányzó pontosvessző
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+
+    //lefoglalt memória felszabadítása
     return 0;
 }
